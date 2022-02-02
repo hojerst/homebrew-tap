49c9527a7ff41a4d8e77a6ae399d121c9e75fbc1
Stefan Hojer <mail@stefanhojer.de>
2022-02-02 23:00:02 +0100

    Initial Commit

---
+++ git-manifest.rb
@@ -0,0 +1,19 @@
+class GitManifest < Formula
+  gm_version = "1.3.1"
+             
+  desc "Manage multiple Git repositories with a manifest git repo"
+  homepage "https://hojerst.gitlab.io/git-manifest/"
+  url "https://gitlab.com/hojerst/git-manifest/-/releases/v#{gm_version}/downloads/git-manifest"
+  version gm_version
+  sha256 "5c23f8de99193925b869608e837267d8a27cfb747e9d608abcb0b50f89a7de1f"
+  license "MIT"
+
+  def install
+    # todo
+  end
+
+  test do
+    v = `#{bin}/git-manifest version`
+    assert_equal gm_version, v.chomp, "git-manifest version not correct expected: #{gmversion}, got: #{v}"
+  end
+end
