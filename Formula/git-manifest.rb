bff93209724550c759eb620a98be4ab7c64aeec9
Stefan Hojer <mail@stefanhojer.de>
2022-02-04 15:55:22 +0100

    moved formula to `Formula` folder

--- Formula/git-manifest.rb
+++ Formula/git-manifest.rb
@@ -1,10 +1,10 @@
 class GitManifest < Formula
-  gm_version = "1.3.1"
+  _version = "1.3.1"
              
   desc "Manage multiple Git repositories with a manifest git repo"
   homepage "https://hojerst.gitlab.io/git-manifest/"
-  url "https://gitlab.com/hojerst/git-manifest/-/releases/v#{gm_version}/downloads/git-manifest"
-  version gm_version
+  url "https://gitlab.com/hojerst/git-manifest/-/releases/v#{_version}/downloads/git-manifest"
+  version _version
   sha256 "5c23f8de99193925b869608e837267d8a27cfb747e9d608abcb0b50f89a7de1f"
   license "MIT"
 
@@ -17,6 +17,6 @@ class GitManifest < Formula
 
   test do
     v = `#{bin}/git-manifest version`
-    assert_equal gm_version, v.chomp, "git-manifest version not correct expected: #{gmversion}, got: #{v}"
+    assert_equal _version, v.chomp, "git-manifest version not correct expected: #{_version}, got: #{v}"
   end
 end
