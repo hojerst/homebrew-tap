46e82b5dc173008b93f1803d77ba509b411e6339
Stefan Hojer <mail@stefanhojer.de>
2022-02-04 15:59:31 +0100

    use `version` directly and add `yq` dependency

--- Formula/git-manifest.rb
+++ Formula/git-manifest.rb
@@ -1,15 +1,14 @@
 class GitManifest < Formula
-  _version = "1.3.1"
-             
+  version "1.3.1"
   desc "Manage multiple Git repositories with a manifest git repo"
   homepage "https://hojerst.gitlab.io/git-manifest/"
-  url "https://gitlab.com/hojerst/git-manifest/-/releases/v#{_version}/downloads/git-manifest"
-  version _version
+  url "https://gitlab.com/hojerst/git-manifest/-/releases/v#{version}/downloads/git-manifest"
   sha256 "5c23f8de99193925b869608e837267d8a27cfb747e9d608abcb0b50f89a7de1f"
   license "MIT"
 
   depends_on "bash"
   depends_on "git"
+  depends_on "yq"
 
   def install
     bin.install "git-manifest"
@@ -17,6 +16,6 @@ class GitManifest < Formula
 
   test do
     v = `#{bin}/git-manifest version`
-    assert_equal _version, v.chomp, "git-manifest version not correct expected: #{_version}, got: #{v}"
+    assert_equal version, v.chomp, "git-manifest version not correct expected: #{version}, got: #{v}"
   end
 end
